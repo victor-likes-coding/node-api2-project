889c8306b27b94fefb1d62610790ce05dcc8acc6
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-19 01:35:58 -0400

    create server and start it

--- api/server.js
+++ api/server.js
@@ -1,2 +1,7 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+
+const server = express();
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require("./api/server.js");
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log("\n* Server Running on http://localhost:4000 *\n");
+});
